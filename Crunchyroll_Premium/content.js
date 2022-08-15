1cb81b6ebc186d9e2dd0cf1517ebb0b49d76b772
GrayDrackman <55756479+GrayDrackman@users.noreply.github.com>
2022-08-14 18:47:13 -0600

    Update content.js

--- Crunchyroll_Premium/content.js
+++ Crunchyroll_Premium/content.js
@@ -117,7 +117,7 @@ function addPlayer(element, playerInfo, beta = false) {
   console.log("[CR Premium] Añadir jwplayer...");
   var ifrm = document.createElement("iframe");
   ifrm.setAttribute("id", "frame");
-  ifrm.setAttribute("src", "https://GrayDrackman.github.io/Practica/");
+  ifrm.setAttribute("src", "https://mateus7g.github.io/crp-iframe-player/");
   ifrm.setAttribute("width", "100%");
   ifrm.setAttribute("height", "100%");
   ifrm.setAttribute("frameborder", "0");
